3befb7e52bb624d7aea00dd7de8afbbbac99c518
Mars_Lee <marskingx@gmail.com>
2019-12-19 13:42:59 +0800

    MoneyBook v1.1 save csv

--- MoneyBook.py
+++ MoneyBook.py
@@ -1,9 +1,15 @@
+#讀取檔案
+with open('moneybook.csv', 'r') as f:
+    for line in f:
+        name, price = line.strip().split(',')
+        print(name, price )
 moneybook = []
 while True:
     name = input('請輸入商品名稱：')
     if name == 'Q':
         break
     price = input('請輸入商品價格：')
+    price = int(price)
     moneybook.append([name, price])
 print(moneybook)
 
@@ -11,5 +17,6 @@ for p in moneybook:
     print(p[0], '的價格是', p[1])
 
 with open ('moneybook.csv', 'w') as f:
+    f.write('商品,價格\n')
     for p in moneybook:
-        f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + str(p[1]) + '\n')
